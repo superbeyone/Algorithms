3373cdca91d184c008a6ae630e81e9cbde9158bb
superbeyone <superbeyone@outlook.com>
2019-01-07 16:24:03 +0800

    practice001

---
+++ src/main/java/com/superbeyone/algorothms/practice001/Solution.java
@@ -0,0 +1,20 @@
+package com.superbeyone.algorothms;
+
+/**
+ * @author Mr.superbeyone
+ * @project algorithms
+ * @className Practice001
+ * @description minimum-depth-of-binary-tree
+ * @create 2019-01-07 15:00
+ **/
+
+public class Practice001 {
+    /**
+     *Given a binary tree, find its minimum depth.
+     *The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/com/superbeyone/algorothms/practice001/TreeNode.java
@@ -0,0 +1,13 @@
+package com.superbeyone.algorothms.practice001;
+
+/**
+ * @author Mr.superbeyone
+ * @project algorithms
+ * @className TreeNode
+ * @description tree
+ * @create 2019-01-07 15:40
+ **/
+
+public class TreeNode {
+
+}
